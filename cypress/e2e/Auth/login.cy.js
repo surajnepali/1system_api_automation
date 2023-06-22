0b297da0cf56af560abc5f9d6fca972b047c1267
Suraj Nepali <surajnepali7896@gmail.com>
2023-06-22 16:13:33 +0545

    fix: bug fix in Login API Testing

--- cypress/e2e/Auth/login.cy.js
+++ cypress/e2e/Auth/login.cy.js
@@ -7,49 +7,49 @@ import SUCCESSFUL from "../../message/successfulMessage";
 describe('Login', () => {
 
     it("Can't login with empty email", () => {
-        login.loginUser('', Cypress.env('password')).then((response) => {
+        login.loginUser('', Cypress.env('password'), 'email').then((response) => {
             expect(response.status).to.eq(400);
             expect(response.body).to.have.property('message', ERROR.emptyEmail);
         });
     });
 
     it("Can't login with empty password", () => {
-        login.loginUser(Cypress.env('registeredEmail'), '').then((response) => {
+        login.loginUser(Cypress.env('registeredEmail'), '', 'email').then((response) => {
             expect(response.status).to.eq(400);
             expect(response.body).to.have.property('message', ERROR.emptyPassword);
         });
     });
 
     it("Can't login with invalid email", () => {
-        login.loginUser('invalidemail', Cypress.env('password')).then((response) => {
+        login.loginUser('invalidemail', Cypress.env('password'), 'email').then((response) => {
             expect(response.status).to.eq(400);
             expect(response.body).to.have.property('message', ERROR.invalidEmail);
         });
     });
 
     it("Can't login with invalid password", () => {
-        login.loginUser(Cypress.env('registeredEmail'), 'invalidpassword').then((response) => {
+        login.loginUser(Cypress.env('registeredEmail'), 'invalidpassword', 'email').then((response) => {
             expect(response.status).to.eq(400);
             expect(response.body).to.have.property('message', ERROR.notMatchedPassword);
         });
     });
 
     it("Can't login with unregistered email", () => {
-        login.loginUser(Cypress.env('newEmail'), Cypress.env('password')).then((response) => {
+        login.loginUser(Cypress.env('newEmail'), Cypress.env('password'), 'email').then((response) => {
             expect(response.status).to.eq(400);
             expect(response.body).to.have.property('message', ERROR.userNotFound);
         });
     });
 
     it("Can't login with password less than 8 characters", () => {
-        login.loginUser(Cypress.env('registeredEmail'), '1234567').then((response) => {
+        login.loginUser(Cypress.env('registeredEmail'), '1234567', 'email').then((response) => {
             expect(response.status).to.eq(400);
             expect(response.body).to.have.property('message', ERROR.invalidPassword);
         });
     });
 
     it("Can login with registered email and password", () => {
-        login.loginUser(Cypress.env('registeredEmail'), Cypress.env('password')).then((response) => {
+        login.loginUser(Cypress.env('registeredEmail'), Cypress.env('password'), 'email').then((response) => {
             expect(response.status).to.eq(200);
             expect(response.body).to.have.property('message', SUCCESSFUL.sucessfulLogin);
         });
