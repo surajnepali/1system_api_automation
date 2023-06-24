f613c696d311fbed43c1129a1a2c24ff76689599
Suraj Nepali <surajnepali7896@gmail.com>
2023-06-24 14:18:13 +0545

    fix: removal of beforeEach() from code

--- cypress/e2e/Auth/registerWithImage.cy.js
+++ cypress/e2e/Auth/registerWithImage.cy.js
@@ -3,33 +3,33 @@
 import register from '../../api/register.api';
 import SUCCESSFUL from '../../message/successfulMessage';
 
+let otp;
 describe('Register Customer', () => {
-  let otp;
 
-  beforeEach(() => {
-    it('Can send otp to new email', () => {
-      register.setOTP(Cypress.env('newEmail'), 'verifyEmail').then((response) => {
-          expect(response.status).to.eq(200);
-          expect(response.body).to.have.property('message',SUCCESSFUL.otpEmailSent);
-          expect(response.body).to.have.property('data');
-          expect(response.body.data).to.have.property('otp');
-          otp = response.body.data.otp;
-          cy.log(otp);
-        });
+
+  it('Can send otp to new email', () => {
+    register.setOTP(Cypress.env('newEmail'), 'verifyEmail').then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property('message',SUCCESSFUL.otpEmailSent);
+        expect(response.body).to.have.property('data');
+        expect(response.body.data).to.have.property('otp');
+        otp = response.body.data.otp;
+        cy.log(otp);
+      });
     });
 
-    it('Can verify otp with valid otp', () => {
-      register
-        .verifyOTP(Cypress.env('newEmail'), 'verifyEmail', otp)
-        .then((response) => {
-          expect(response.status).to.eq(200);
-          expect(response.body).to.have.property(
-            'message',
-            SUCCESSFUL.otpVerified
-          );
-        });
-    });
+  it('Can verify otp with valid otp', () => {
+    register
+      .verifyOTP(Cypress.env('newEmail'), 'verifyEmail', otp)
+      .then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property(
+          'message',
+          SUCCESSFUL.otpVerified
+        );
+      });
   });
+
 
   it('should throw status code of 400', () => {
     cy.fixture('sample.jpg', 'binary')
@@ -51,7 +51,7 @@ describe('Register Customer', () => {
   });
 
   it('should throw status code of 200', () => {
-    cy.fixture('sample.jpg', 'binary')
+    cy.fixture('person.jpg', 'binary')
       .then((file) => Cypress.Blob.binaryStringToBlob(file, 'image/jpg'))
       .then((blob) => {
         let formData = new FormData();
@@ -60,7 +60,7 @@ describe('Register Customer', () => {
         formData.append('username', 'John Doe');
         formData.append('otp', otp);
         formData.append('estimated_service_usage', 12);
-        formData.append('profile_picture', blob, 'sample.jpg');
+        formData.append('profile_picture', blob, 'person.jpg');
         formData.append('loginAgent', 'email');
 
         register.registerCustomerWithImage(formData).then((response) => {
